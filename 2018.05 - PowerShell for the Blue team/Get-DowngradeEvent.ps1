963062506520a4e46d973d360002309d25f41ffd
Martin Schvartzman <martin77s@gmail.com>
2019-12-15 23:29:12 +0200

    content updates

--- 2018.05 - PowerShell for the Blue team/Get-DowngradeEvent.ps1
+++
@@ -1,5 +0,0 @@
-Get-WinEvent -LogName "Windows PowerShell" |
-    Where-Object Id -eq 400 | Foreach-Object {
-        $version = [Version] ($_.Message -replace '(?s).*EngineVersion=([\d\.]+)*.*','$1')
-        if($version -lt ([Version] "5.0")) { $_ }
-}
--- 2018.05 - PowerShell for the Blue team/Get-PSHosts.ps1
+++
@@ -1,9 +0,0 @@
-param($Path = $pwd.Path)
-dir $Path -Recurse -Include '*.exe', '*.dll' -ea 0 -pv file | % {
-    try {
-        $asm = [Reflection.Assembly]::ReflectionOnlyLoadFrom($_.FullName)
-        if ($asm.GetReferencedAssemblies().Name -contains 'System.Management.Automation') {
-            $_.FullName
-        }
-    } catch {}
-}
